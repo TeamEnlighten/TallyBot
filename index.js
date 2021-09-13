c9f4aae55786f0463e18aaf48e942e9fd81ee874
TeamEnlighten <87457810+TeamEnlighten@users.noreply.github.com>
2021-09-13 00:00:45 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
-const config = require('./config.json')
 const mongo = require('./schema/mongo')
 const loadCommands = require('./commands/load-commands')
 
